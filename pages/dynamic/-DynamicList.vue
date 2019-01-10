f1de737a7bb029f8f5c102576436f4d51fb212b4
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-01-10 17:53:54 +0900

    update remove wrapper

--- pages/dynamic/-DynamicList.vue
+++ pages/dynamic/-DynamicList.vue
@@ -1,17 +1,16 @@
 <template lang="pug">
-  div
-    ul.items
-      li.item(
-        v-for="(item, index) in items"
-        :key="index"
-      )
-        nuxt-link(
-          :to=`{
-            name: 'dynamic-id',
-            path: '/dynamic/:id',
-            params: { id: item.id }
-          }`
-        ) {{ item.id }}
+  ul.items
+    li.item(
+      v-for="(item, index) in items"
+      :key="index"
+    )
+      nuxt-link(
+        :to=`{
+          name: 'dynamic-id',
+          path: '/dynamic/:id',
+          params: { id: item.id }
+        }`
+      ) {{ item.id }}
 </template>
 
 <script>
